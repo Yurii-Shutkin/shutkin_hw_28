819991beb372af8176a392467fdee5b6ef1868f1
Yurii-Shutkin <itorik@ukr.net>
2021-12-06 07:22:10 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+const calculator = {
+    read() {
+        const a = prompt('Первое значение');
+        const b = prompt('Второе значение');
+        if(!isNaN(a) && !isNaN(b) && a.trim() && b.trim()) {
+            this.a = +a;
+            this.b = +b;
+        } else {
+            alert('Мне нужны числа. Попробуй еще раз');
+            this.read();
+        }
+    },
+    sum() {
+        return this.a + this.b;
+    },
+    mul() {
+        return this.a * this.b;
+    },
+}
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
